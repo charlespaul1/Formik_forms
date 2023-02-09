55eeb5ee6b94db019057bb45139c791b71f651af
charlespaul1 <charlespaulwabomba@gmail.com>
2023-02-09 08:02:01 +0300

    handling visited fields

--- simpleform/src/components/SimpleForm.js
+++ simpleform/src/components/SimpleForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useFormik } from 'formik';
 const initialValues = {
-  name: '',
+  name: 'cp',
   email: '',
   city: ''
 }
@@ -26,7 +26,7 @@ const validate = values => {
           if (!values.city) {
             errors.city = 'Required'
           }
-          
+
           return errors
 }
 function SimpleForm() {
@@ -35,29 +35,42 @@ function SimpleForm() {
       onSubmit,
       validate
     })
-    //console.log('Form values', formik.values)
+    console.log('visited fields', formik.touched)
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
+        
+        <div className='form-control'>
         <label htmlFor='name'>Name</label>
         <input type='text' 
         id='name' name='name' 
         onChange={formik.handleChange} 
+        onBlur={formik.handleBlur}
         value={formik.values.name}/>
+        {formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+        </div>
+        
 
+        <div className='form-control'>
         <label htmlFor='email'>E-mail</label>
         <input type='email' 
         id='email' 
         name='email' 
         onChange={formik.handleChange} 
+        onBlur={formik.handleBlur}
         value={formik.values.email}/>
-
+        {formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
+          </div>
+        <div className='form-control'>
         <label htmlFor='city'>City</label>
         <input type='text' 
         id='city' name='city' 
         onChange={formik.handleChange} 
+        onBlur={formik.handleBlur}
         value={formik.values.city}/>
+        {formik.errors.city ? <div className='error'>{formik.errors.city}</div> : null}
 
+        </div>
         <button type='submit'>Submit</button>
       </form>
     </div>
